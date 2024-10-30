043dd45bfb93dd9ba8fe6fb58681c189123bd1eb
Ante Borzić <aborzic@extensionengine.com>
2024-10-30 10:24:23 +0100

    feat: fix footer rendering - stick to bottom on no content - handle smaller screen sizes

--- apps/platform/src/app/components/footer/Footer.tsx
+++ apps/platform/src/app/components/footer/Footer.tsx
@@ -4,8 +4,8 @@ import FooterLinks from '@/app/components/footer/FooterLinks';
 
 export default function Footer() {
 	return (
-		<footer className='bg-[#F6F6F6] py-12'>
-			<Container className='mx-auto flex flex-col items-center justify-between px-5 py-5 lg:flex-row'>
+		<footer className='bg-[#F6F6F6] py-4 lg:py-12'>
+			<Container className='mx-auto flex flex-col-reverse items-center justify-between px-3 py-2 md:px-5 md:py-5 md:flex-row gap-2 md:gap-0'>
 				<Socials />
 				<FooterLinks />
 			</Container>
